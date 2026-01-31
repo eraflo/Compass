a1f133d7b1c842f2e0eddcce8a70fde58d6465be
Florian Maillard <81640753+eraflo@users.noreply.github.com>
2026-01-31 23:03:21 +0100

    fix: use on only win and mac

--- src/core/docker.rs
+++ src/core/docker.rs
@@ -12,7 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use anyhow::{Context, Result, bail};
+#[cfg(any(target_os = "windows", target_os = "macos"))]
+use anyhow::Context;
+use anyhow::{Result, bail};
 use std::process::Command;
 
 /// Checks if Docker is available and running.
